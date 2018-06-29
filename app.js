c28a6bf25d84fc1f405050cb4d717355e07c93f4
irohitb <irohitbhatia@gmail.com>
2018-06-29 17:34:13 +0530

    Index page and app.js completed

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-var profilesNumbers;
+var profiles;
 var message;
 
 document.getElementById("linkedin-start").addEventListener("click", function() {
-  profiles = document.getElementById("LinkedIn-number").value;
-  message = document.getElementById("LinkedIn-Message").value;
 
-  if (profiles || profiles>0) {
+  profiles = document.getElementById("LinkedIn-number").value
+
+    message = document.getElementById("LinkedIn-Message").innerHTML;
+
+  console.log(profiles)
+  console.log(null)
+
+  if (profiles > 0 && message != null) {
+    alert("Do not close, minimize and work on new tab while extension is working, Open new window to browse the web")
+    document.getElementById("linkedin-start").disabled = true;
+    document.getElementById('LinkedIn-number').disabled = true;
+    document.getElementById("LinkedIn-Message").disabled = true;
+    document.getElementById("linkedin-stop").disabled = false;
     chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {todo: "startAdding", profiles: profiles, message:message})
+    chrome.tabs.sendMessage(tabs[0].id, {todo: "startAddingwithMessage", profiles: profiles, message:message})
+
     });
   }
+
+  if (profiles > 0 && message == null) {
+    alert("No message written! Do not close, minimize and work on new tab while extension is working, Open new window to browse the web")
+    document.getElementById("linkedin-start").disabled = true;
+    document.getElementById('LinkedIn-number').disabled = true;
+    document.getElementById("LinkedIn-Message").disabled = true;
+    document.getElementById("linkedin-stop").disabled = false;
+    chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {todo: "startAddingWithoutMessage", profiles: profiles})
+    });
+  }
+
+  if (profiles <= 0) {
+    alert("Number of invitation to be sent can't be negative")
+  }
 })
+
+document.getElementById("linkedin-stop").addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {todo: "stopEverything"})
+    document.getElementById("linkedin-start").disabled = false;
+    document.getElementById("linkedin-stop").disabled = true;
+  });
+});
