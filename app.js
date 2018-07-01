7d1fdfa7f45d8f68af9134fe0f3331d8ac36cfb6
irohitb <irohitbhatia@gmail.com>
2018-07-01 20:09:18 +0530

    sending invites improvement

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ document.getElementById("linkedin-start").addEventListener("click", function() {
     document.getElementById("linkedin-stop").disabled = false;
     chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {todo: "startAddingwithMessage", profiles: profiles, message:message})
-
     });
   }
 
@@ -31,7 +30,7 @@ document.getElementById("linkedin-start").addEventListener("click", function() {
   }
 
   if (profiles <= 0) {
-    alert("Number of invitation to be sent can't be negative")
+    alert("Number of invitation to be sent can't be negative or zero")
   }
 })
 
@@ -43,19 +42,45 @@ document.getElementById("linkedin-stop").addEventListener("click", function() {
   });
 });
 
+
+
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-      console.log("Message was here")
   if (request.todo == 'running') {
-    console.log("Message is here")
-    document.getElementById("linkedin-start").disabled = true;
-    document.getElementById('LinkedIn-number').disabled = true;
-    document.getElementById("LinkedIn-Message").disabled = true;
+    console.log(request.connectionAdded)
+
+    document.getElementById('LinkedIn-number').style.display = "none";
+    document.getElementById("LinkedIn-Message").style.display = "none";
+
+    document.getElementById("instruction").style.display = "none";
+    document.getElementById("instruction1").style.display = "none";
+
+    document.getElementById("linkedin-start").style.display = "none";
+    document.getElementById("linkedin-stop").style.display = "block"
     document.getElementById("linkedin-stop").disabled = false;
+
+    document.getElementById("InvitesSent").style.display = "block";
+    document.getElementById("AddedNumber").innerHTML = request.connectionAdded
+
+    document.getElementById("messageText").style.display="none";
+    document.getElementById("numberText").style.display="none";
   }
   if (request.todo == 'stopped') {
     document.getElementById("linkedin-start").disabled = false;
     document.getElementById('LinkedIn-number').disabled = false;
     document.getElementById("LinkedIn-Message").disabled = false;
     document.getElementById("linkedin-stop").disabled = true;
+    document.getElementById("InvitesSent").style.display = "none";
+    document.getElementById("AddedNumber").value = "";
   }
 })
+
+/*
+document.getElementById("LinkedIn-Message").placeholder = request.message;
+document.getElementById('LinkedIn-number').placeholder = request.numberOfConnectionsToBeAdded;
+document.getElementById("LinkedIn-Message").placeholder = true;
+document.getElementById("linkedin-start").disabled = true;
+document.getElementById('LinkedIn-number').disabled = true;
+document.getElementById("LinkedIn-Message").disabled = true;
+document.getElementById("linkedin-stop").disabled = false;
+*/
--- scripts/content.js
+++ scripts/content.js
@@ -10,7 +10,8 @@ if (request.todo == "startAddingwithMessage") {
 
     window.scrollTo(0,document.body.scrollHeight);
     var i = 0;
-    var j = i+1
+    var connectionAdded = i+1;
+    var numberOfConnectionsToBeAdded = request.profiles;
     var buttonA;
     var message = request.message;
     var message1;
@@ -20,49 +21,41 @@ if (request.todo == "startAddingwithMessage") {
 
     setTimeout(function() {
       lengthoFConnectButton =  document.querySelectorAll('button[data-control-name="srp_profile_actions"]').length
-       console.log(lengthoFConnectButton)
     }, 700)
 
     var interval = setInterval(function(){
 
               setTimeout(function() {
-              chrome.runtime.sendMessage({todo:'running'});
+              chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded});
               i++;
-              j++;
-              console.log(i)
-              console.log(j)
+              connectionAdded++;
+              console.log(connectionAdded)
               buttonA = document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i]
               buttonA = buttonA.getAttribute('aria-label');
-              console.log("this is buttonA")
-              console.log(buttonA)
               btnr = buttonA.split("Connect with ").pop();
-              console.log("THis is btnr")
-              console.log(btnr)
               FullName = btnr
-              console.log("THis is Full Name")
-              console.log(FullName)
               const regex   = /\{FullName\}/gi;
               message1 = message.replace(regex, FullName)
               document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i].click()
-            }, 2000)
+            }, 1200)
 
               setTimeout(function(){
-              chrome.runtime.sendMessage({todo:'running'});
-              console.log(message1)
+              chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded});
               document.querySelector('button[class="button-secondary-large mr1"]').click()
               document.getElementById('custom-message').value = message1;
-            }, 6000)
+            }, 1700)
 
             setTimeout(function(){
-                chrome.runtime.sendMessage({todo:'running'});
+            chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded});
             document.querySelector('button[class="button-primary-large ml1"]').click()
-          }, 7000)
+          }, 2200)
 
-          if (j == lengthoFConnectButton) {
+          if (connectionAdded == lengthoFConnectButton) {
+            chrome.runtime.sendMessage({todo:'stopped'});
             clearInterval(interval);
             }
 
-      }, 8000)
+      }, 2500)
 }
 
 
