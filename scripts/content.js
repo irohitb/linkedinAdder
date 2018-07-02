2d978c0a1f2632ee782949aa1f2959dff3bc7244
irohitb <irohitbhatia@gmail.com>
2018-07-03 03:22:27 +0530

    Fixed everything, everything

--- scripts/content.js
+++ scripts/content.js
@@ -8,9 +8,10 @@ chrome.runtime.sendMessage({todo:'ShowPageAction'});
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
   if (request.todo == "startAddingwithMessage") {
-
+      window.scrollTo(0,document.body.scrollHeight);
       //Vaiables Declaration
       var i = 0;
+      var connectionAddedinThisSession = 0;
       var connectionAdded = 0;
       var numberOfConnectionsToBeAdded = request.profiles;
       var buttonA;
@@ -26,6 +27,29 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       var remainingConnectionsToBeAdded;
       //var remaingConnectionsToBeAdded
 
+//Functions
+function nextPageClick () {
+      i=0;
+      var x = document.getElementsByClassName("page-list")[0];
+      x = x.querySelectorAll('li')[nextPage]
+      x.querySelectorAll('button')[0].click()
+
+      var checkClassExsist = setInterval(function() {
+        //checking if the page is loaded or not
+        classExsist = document.getElementsByClassName('search-is-loading').length
+        if (classExsist == 0) {
+          window.scrollTo(0,document.body.scrollHeight);
+          setTimeout(function() {
+            lengthoFConnectButton =  document.querySelectorAll('button[data-control-name="srp_profile_actions"]').length
+            if (lengthoFConnectButton >  remainingConnectionsToBeAdded) {
+              lengthoFConnectButton = remainingConnectionsToBeAdded
+            }
+            connectionAdder();
+          }, 500)
+          clearInterval(checkClassExsist);
+        }
+    }, 600)
+}
       //Finding and Incrementing the page Number of current activeClass
       function cureentActivePageNumber () {
       const el = document.querySelector("li.active");
@@ -36,28 +60,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       }
 
   //clicking on Nextpage button and calling sending invite function
-      function nextPageClick () {
-            i=0;
-            var x = document.getElementsByClassName("page-list")[0];
-            x = x.querySelectorAll('li')[nextPage]
-            x.querySelectorAll('button')[0].click()
+      function nextPageToggler() {
+          console.log("nextPage Toggler")
+            remainingConnectionsToBeAdded = numberOfConnectionsToBeAdded - connectionAdded;
+            lengthoFConnectButton = 0;
+            connectionAddedinThisSession = 0;
 
-            var checkClassExsist = setInterval(function() {
-              //checking if the page is loaded or not
-              classExsist = document.getElementsByClassName('search-is-loading').length
-              if (classExsist == 0) {
-                window.scrollTo(0,document.body.scrollHeight);
-                setTimeout(function() {
-                  lengthoFConnectButton =  document.querySelectorAll('button[data-control-name="srp_profile_actions"]').length
-                  if (lengthoFConnectButton >  remainingConnectionsToBeAdded) {
-                    lengthoFConnectButton = remainingConnectionsToBeAdded
-                  }
-                  connectionAdder();
-                }, 500)
-                clearInterval(checkClassExsist);
-              }
-          }, 600)
-      }
+            clearInterval(interval);
+
+            setTimeout(function() {
+              cureentActivePageNumber();
+            }, 1000) //THis will call new page click function
+
+          }
 
 //This scrolls the page down at starting
       setTimeout(function() {
@@ -67,24 +82,36 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         }
       }, 400)
 
-    function stopped() {
-      alert("Invitation sent Complete")
-      chrome.runtime.sendMessage({todo:'stopped'});
-      clearInterval(interval)
-    }
+
 
 
 //This starts sending invites
 function connectionAdder () {
       var interval = setInterval(function(){
 
+        function stopped() {
+          alert("Invitation sent Complete :)")
+          chrome.runtime.sendMessage({todo:'stopped'});
+          clearInterval(interval)
+        }
 
+        function nextPageToggler() {
+            console.log("nextPage Toggler")
+              remainingConnectionsToBeAdded = numberOfConnectionsToBeAdded - connectionAdded;
+              lengthoFConnectButton = 0;
+              connectionAddedinThisSession = 0;
 
+              clearInterval(interval);
 
+              setTimeout(function() {
+                cureentActivePageNumber();
+              }, 1000) //THis will call new page click function
+
+            }
 //SET TIMEOUT FUNCTIONS
                 setTimeout(function() {
-                  if (connectionAdded < numberOfConnectionsToBeAdded && connectionAdded < lengthoFConnectButton) {
-                    console.log("Connect button")
+                  if (connectionAdded < numberOfConnectionsToBeAdded && connectionAddedinThisSession < lengthoFConnectButton) {
+                    console.log("Connect button inside")
                     chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName});
                     buttonA = document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i]
                     buttonA = buttonA.getAttribute('aria-label');
@@ -93,71 +120,57 @@ function connectionAdder () {
                     const regex   = /\{FullName\}/gi;
                     message1 = message.replace(regex, FullName)
                     document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i].click()
-                  } else {
-                  stopped();
-                }
+                  }
+                  if (connectionAdded == numberOfConnectionsToBeAdded) {
+                    stopped()
+                  }
+                  if (connectionAddedinThisSession >= lengthoFConnectButton && connectionAdded < numberOfConnectionsToBeAdded) {
+                    nextPageToggler()
+                  }
                 }, 1200)
 
                 setTimeout(function(){
-                  if (connectionAdded < numberOfConnectionsToBeAdded && connectionAdded < lengthoFConnectButton) {
+                  if (connectionAdded < numberOfConnectionsToBeAdded && connectionAddedinThisSession < lengthoFConnectButton) {
+                    console.log("Messages button inside")
                     chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName  });
                     document.querySelector('button[class="button-secondary-large mr1"]').click()
                     document.getElementById('custom-message').value = message1;
-                  } else {
-                    stopped();
                   }
                 }, 1700)
 
               setTimeout(function(){
-                if (connectionAdded < numberOfConnectionsToBeAdded && connectionAdded < lengthoFConnectButton) {
+                if (connectionAdded < numberOfConnectionsToBeAdded && connectionAddedinThisSession < lengthoFConnectButton) {
+                  console.log("Send Invite button inside")
                   chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName});
                   document.querySelector('button[class="button-primary-large ml1"]').click()
                   i++;
                   connectionAdded++;
-                } else {
-                  stopped();
+                  connectionAddedinThisSession++;
                 }
               }, 2200)
-
-//IF CONDITIONS
-
-            //If no of people on the page to be added are more than connect button
-            if (connectionAdded == lengthoFConnectButton && numberOfConnectionsToBeAdded > connectionAdded) {
-                remainingConnectionsToBeAdded = numberOfConnectionsToBeAdded - connectionAdded;
-                lengthoFConnectButton = 0;
-
-                clearInterval(interval);
-
-                setTimeout(function() {
-                  cureentActivePageNumber();
-                }, 1000) //THis will call new page click function
-
-              } else {
-              alert("Invitation sent Complete")
-              chrome.runtime.sendMessage({todo:'stopped'});
-              clearInterval(interval);
-              }
 
 
             //THis will stop everything once the stop button is pressed and send a message to App.js to update the UI
               chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-              if (request.todo === "stopEverything") {
-                chrome.runtime.sendMessage({todo:'stopped'});
-                clearInterval(interval);
-              }
-            })
+                if (request.todo === "stopEverything") {
+                  chrome.runtime.sendMessage({todo:'stopped'});
+                  clearInterval(interval);
+                }
+              })
 
         }, 2500)
       }
 
-    connectionAdder(); //This will start adding people
-  }
-})
+    connectionAdder();
+  } //This will start adding people
 
 
-  /*if (request.todo == "startAddingWithoutMessage") {
 
+  if (request.todo == "startAddingWithoutMessage") {
+    window.scrollTo(0,document.body.scrollHeight);
+    //Vaiables Declaration
     var i = 0;
+    var connectionAddedinThisSession = 0;
     var connectionAdded = 0;
     var numberOfConnectionsToBeAdded = request.profiles;
     var buttonA;
@@ -170,9 +183,32 @@ function connectionAdder () {
     var classExsist
     var cureentActivePage;
     var nextpageList
-    var remainingConnections;
+    var remainingConnectionsToBeAdded;
     //var remaingConnectionsToBeAdded
 
+//Functions
+function nextPageClick () {
+    i=0;
+    var x = document.getElementsByClassName("page-list")[0];
+    x = x.querySelectorAll('li')[nextPage]
+    x.querySelectorAll('button')[0].click()
+
+    var checkClassExsist = setInterval(function() {
+      //checking if the page is loaded or not
+      classExsist = document.getElementsByClassName('search-is-loading').length
+      if (classExsist == 0) {
+        window.scrollTo(0,document.body.scrollHeight);
+        setTimeout(function() {
+          lengthoFConnectButton =  document.querySelectorAll('button[data-control-name="srp_profile_actions"]').length
+          if (lengthoFConnectButton >  remainingConnectionsToBeAdded) {
+            lengthoFConnectButton = remainingConnectionsToBeAdded
+          }
+          connectionAdder();
+        }, 500)
+        clearInterval(checkClassExsist);
+      }
+  }, 600)
+}
     //Finding and Incrementing the page Number of current activeClass
     function cureentActivePageNumber () {
     const el = document.querySelector("li.active");
@@ -183,101 +219,107 @@ function connectionAdder () {
     }
 
 //clicking on Nextpage button and calling sending invite function
-    function nextPageClick () {
-          i=0;
-          var x = document.getElementsByClassName("page-list")[0];
-          x = x.querySelectorAll('li')[nextPage]
-          x.querySelectorAll('button')[0].click()
+    function nextPageToggler() {
+        console.log("nextPage Toggler")
+          remainingConnectionsToBeAdded = numberOfConnectionsToBeAdded - connectionAdded;
+          lengthoFConnectButton = 0;
+          connectionAddedinThisSession = 0;
 
-          var checkClassExsist = setInterval(function() {
-            //checking if the page is loaded or not
-            classExsist = document.getElementsByClassName('search-is-loading').length
-            if (classExsist == 0) {
-              window.scrollTo(0,document.body.scrollHeight);
-              setTimeout(function() {
-                lengthoFConnectButton =  document.querySelectorAll('button[data-control-name="srp_profile_actions"]').length
-                connectionAdder();
-              }, 500)
-              clearInterval(checkClassExsist);
-            }
-        }, 600)
-    }
+          clearInterval(interval);
+
+          setTimeout(function() {
+            cureentActivePageNumber();
+          }, 1000) //THis will call new page click function
+
+        }
 
 //This scrolls the page down at starting
     setTimeout(function() {
       lengthoFConnectButton =  document.querySelectorAll('button[data-control-name="srp_profile_actions"]').length
+      if (lengthoFConnectButton > numberOfConnectionsToBeAdded ) {
+        lengthoFConnectButton = numberOfConnectionsToBeAdded;
+      }
     }, 400)
+
+
 
 
 //This starts sending invites
 function connectionAdder () {
     var interval = setInterval(function(){
 
-      if (lengthoFConnectButton > numberOfConnectionsToBeAdded ) {
-        lengthoFConnectButton = numberOfConnectionsToBeAdded;
+      function stopped() {
+        alert("Invitation sent Complete :)")
+        chrome.runtime.sendMessage({todo:'stopped'});
+        clearInterval(interval)
       }
 
+      function nextPageToggler() {
+          console.log("nextPage Toggler")
+            remainingConnectionsToBeAdded = numberOfConnectionsToBeAdded - connectionAdded;
+            lengthoFConnectButton = 0;
+            connectionAddedinThisSession = 0;
+
+            clearInterval(interval);
+
+            setTimeout(function() {
+              cureentActivePageNumber();
+            }, 1000) //THis will call new page click function
+
+          }
+//SET TIMEOUT FUNCTIONS
               setTimeout(function() {
-              chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName  });
-              buttonA = document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i]
-              buttonA = buttonA.getAttribute('aria-label');
-              btnr = buttonA.split("Connect with ").pop();
-              FullName = btnr
-              const regex   = /\{FullName\}/gi;
-              document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i].click()
-            }, 1200)
+                if (connectionAdded < numberOfConnectionsToBeAdded && connectionAddedinThisSession < lengthoFConnectButton) {
+                  console.log("Connect button inside")
+                  chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName});
+                  buttonA = document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i]
+                  buttonA = buttonA.getAttribute('aria-label');
+                  btnr = buttonA.split("Connect with ").pop();
+                  FullName = btnr
+                  document.querySelectorAll('button[data-control-name="srp_profile_actions"]')[i].click()
+                }
+                if (connectionAdded == numberOfConnectionsToBeAdded) {
+                  stopped()
+                }
+                if (connectionAddedinThisSession >= lengthoFConnectButton && connectionAdded < numberOfConnectionsToBeAdded) {
+                  nextPageToggler()
+                }
+              }, 1200)
 
               setTimeout(function(){
-              chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName  });
-              document.querySelector('button[class="button-secondary-large mr1"]').click()
-          }, 1700)
+                if (connectionAdded < numberOfConnectionsToBeAdded && connectionAddedinThisSession < lengthoFConnectButton) {
+                  console.log("Messages button inside")
+                  chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName  });
+                  document.querySelector('button[class="button-secondary-large mr1"]').click()
+                }
+              }, 1700)
 
             setTimeout(function(){
-            chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName});
-            document.querySelector('button[class="button-primary-large ml1"]').click()
-            i++;
-            connectionAdded++;
+              if (connectionAdded < numberOfConnectionsToBeAdded && connectionAddedinThisSession < lengthoFConnectButton) {
+                console.log("Send Invite button inside")
+                chrome.runtime.sendMessage({todo:'running', connectionAdded:connectionAdded, message:message, numberOfConnectionsToBeAdded: numberOfConnectionsToBeAdded, FullName:FullName});
+                document.querySelector('button[class="button-primary-large ml1"]').click()
+                i++;
+                connectionAdded++;
+                connectionAddedinThisSession++;
+              }
+            }, 2200)
 
-          }, 2200)
-
-          //If no of people on the page to be added are less than connect button
-
-
-          if (connectionAdded > numberOfConnectionsToBeAdded) {
-            alert("Invitation sent Complete")
-            chrome.runtime.sendMessage({todo:'stopped'});
-            clearInterval(interval);
-          }
-
-          //If no of people on the page to be added are more than connect button
-          if (connectionAdded == lengthoFConnectButton) {
-            if (numberOfConnectionsToBeAdded > lengthoFConnectButton) {
-              clearInterval(interval);
-              setTimeout(function() {
-              cureentActivePageNumber();
-            }, 2400) //THis will call new page click function
-            } else {
-            alert("Invitation sent Complete")
-            chrome.runtime.sendMessage({todo:'stopped'});
-            clearInterval(interval);
-            }
-          }
 
           //THis will stop everything once the stop button is pressed and send a message to App.js to update the UI
             chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-            if (request.todo === "stopEverything") {
-              chrome.runtime.sendMessage({todo:'stopped'});
-              clearInterval(interval);
-            }
-          })
+              if (request.todo === "stopEverything") {
+                chrome.runtime.sendMessage({todo:'stopped'});
+                clearInterval(interval);
+              }
+            })
 
       }, 2500)
     }
-
-  connectionAdder(); //This will start adding people
+  connectionAdder();
   }
 })
 
 
-*/
+
 //Hey! {FullName}. Looking forward to having you in my professional network
